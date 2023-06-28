412f99b715e55ec50ff825c8087bfea72cd3fdef
David Maciejewski <david_maciejewski@web.de>
2023-06-28 16:57:19 +0200

    I made it a bit worse for more refactoring

--- RefactoringProject/Program.cs
+++ RefactoringProject/Program.cs
@@ -8,11 +8,11 @@ using System.Threading;
 
 namespace RefactoringProject
 {
-    struct Position
+    struct Pos
     {
         public int row;
         public int col;
-        public Position(int row, int col)
+        public Pos(int row, int col)
         {
             this.row = row;
             this.col = col;
@@ -22,53 +22,53 @@ namespace RefactoringProject
     {
         static void Main(string[] args)
         {
-            int lastFoodTime = 0;
+            int lft = 0; // lastFoodTime
             int FoodDisspearTime = 8000;
             int negativePoints = 0;
 
 
 
-            Position[] directions = new Position[] // block positions
+            Pos[] directions = new Pos[] // block positions
             {
-                new Position(0, 1), // right
-                new Position(0, -1), // left
-                new Position(1, 0), //down
-                new Position(-1, 0), //up
+                new Pos(0, 1), // right
+                new Pos(0, -1), // left
+                new Pos(1, 0), //down
+                new Pos(-1, 0), //up
             };
             double sleeptime = 100;
-            int direction = 0; //0
-            Random randomNubersGenerator = new Random();
+            int dir = 0; //dir = 0
+            Random rng = new Random();
             Console.BufferHeight = Console.WindowHeight;
             Console.BufferWidth = Console.WindowWidth;
-            Position food = new Position(randomNubersGenerator.Next(0, Console.WindowHeight),
-                randomNubersGenerator.Next(0, Console.WindowWidth));
+            Pos f = new Pos(rng.Next(0, Console.WindowHeight),
+                rng.Next(0, Console.WindowWidth)); // food
 
-            lastFoodTime = Environment.TickCount;
-            Console.SetCursorPosition(food.col, food.row);
+            lft = Environment.TickCount;
+            Console.SetCursorPosition(f.col, f.row);
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("@");
 
-            List<Position> obstacles = new List<Position>();
+            List<Pos> obs = new List<Pos>();
             {
-                new Position(12, 21);
-                new Position(12, 21);
-                new Position(21, 6);
+                new Pos(12, 21);
+                new Pos(12, 21);
+                new Pos(21, 6);
             }
-            foreach (Position obstacle in obstacles)
+            foreach (Pos o in obs)
             {
                 Console.ForegroundColor = ConsoleColor.Blue;
-                Console.SetCursorPosition(obstacle.col, obstacle.row);
+                Console.SetCursorPosition(o.col, o.row);
                 Console.Write("M");
             }
 
-            Queue<Position> snakeelements = new Queue<Position>();
+            Queue<Pos> se = new Queue<Pos>(); // snake elements
             for (int i = 0; i <= 5; i++)
             {
-                snakeelements.Enqueue(new Position(0, i));
+                se.Enqueue(new Pos(0, i));
             }
-            foreach (Position position in snakeelements)
+            foreach (Pos p in se)
             {
-                Console.SetCursorPosition(position.col, position.row);
+                Console.SetCursorPosition(p.col, p.row);
                 Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.Write("G");
             }
@@ -79,52 +79,52 @@ namespace RefactoringProject
 
                 if (Console.KeyAvailable)
                 {
-                    ConsoleKeyInfo userInput = Console.ReadKey();
-                    if (userInput.Key == ConsoleKey.LeftArrow)
+                    ConsoleKeyInfo ui = Console.ReadKey();
+                    if (ui.Key == ConsoleKey.LeftArrow)
                     {
-                        if (direction != 0) direction = 1;
+                        if (dir != 0) dir = 1;
                     }
-                    if (userInput.Key == ConsoleKey.RightArrow)
+                    if (ui.Key == ConsoleKey.RightArrow)
                     {
-                        if (direction != 1) direction = 0;
+                        if (dir != 1) dir = 0;
                     }
-                    if (userInput.Key == ConsoleKey.UpArrow)
+                    if (ui.Key == ConsoleKey.UpArrow)
                     {
-                        if (direction != 2) direction = 3;
+                        if (dir != 2) dir = 3;
                     }
-                    if (userInput.Key == ConsoleKey.DownArrow)
+                    if (ui.Key == ConsoleKey.DownArrow)
                     {
-                        if (direction != 3) direction = 2;
+                        if (dir != 3) dir = 2;
                     }
                 }
-                Position snakeHead = snakeelements.Last();
-                Position nextDirections = directions[direction];
-                Position snakeNewHead = new Position(snakeHead.row + nextDirections.row,
-                    snakeHead.col + nextDirections.col);
+                Pos sh = se.Last(); //snake head
+                Pos nDir = directions[dir]; // next direction
+                Pos snh = new Pos(sh.row + nDir.row,
+                    sh.col + nDir.col); // snake new head
 
-                if (snakeNewHead.col < 0) snakeNewHead.col = Console.WindowWidth - 1;
-                if (snakeNewHead.row < 0) snakeNewHead.row = Console.WindowWidth - 1;
-                if (snakeNewHead.row >= Console.WindowHeight) snakeNewHead.row = 0;
-                if (snakeNewHead.col >= Console.WindowWidth) snakeNewHead.col = 0;
+                if (snh.col < 0) snh.col = Console.WindowWidth - 1;
+                if (snh.row < 0) snh.row = Console.WindowWidth - 1;
+                if (snh.row >= Console.WindowHeight) snh.row = 0;
+                if (snh.col >= Console.WindowWidth) snh.col = 0;
 
-                if (snakeelements.Contains(snakeNewHead))
+                if (se.Contains(snh))
                 {
                     Console.SetCursorPosition(0, 0);
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Game olver!");
-                    int usersPoints = ((snakeelements.Count - 6) * 100) - negativePoints;
+                    int usersPoints = ((se.Count - 6) * 100) - negativePoints;
                     usersPoints = Math.Max(usersPoints, 0); // взима по голямата стойност
                     Console.ForegroundColor = ConsoleColor.DarkRed;
                     Console.WriteLine("Your points are : {0}", usersPoints);
                     return;
                 }
 
-                Console.SetCursorPosition(snakeHead.col, snakeHead.row);
+                Console.SetCursorPosition(sh.col, sh.row);
                 Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.Write("G");
 
-                snakeelements.Enqueue(snakeNewHead);
-                Console.SetCursorPosition(snakeNewHead.col, snakeNewHead.row);
+                se.Enqueue(snh);
+                Console.SetCursorPosition(snh.col, snh.row);
                 //if (direction == 0) Console.Write(">");
                 // if (direction == 1) Console.Write("<");
                 // if (direction == 3) Console.Write("^");
@@ -132,17 +132,17 @@ namespace RefactoringProject
                 Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.Write("G");
 
-                if (snakeNewHead.col == food.col && snakeNewHead.row == food.row)
+                if (snh.col == f.col && snh.row == f.row)
                 {
                     do
                     {
                         // feeding yhe snake 
-                        food = new Position(randomNubersGenerator.Next(0, Console.WindowHeight),
-                        randomNubersGenerator.Next(0, Console.WindowWidth));
+                        f = new Pos(rng.Next(0, Console.WindowHeight),
+                        rng.Next(0, Console.WindowWidth));
                     }
-                    while (snakeelements.Contains(food));
-                    lastFoodTime = Environment.TickCount;
-                    Console.SetCursorPosition(food.col, food.row);
+                    while (se.Contains(f));
+                    lft = Environment.TickCount;
+                    Console.SetCursorPosition(f.col, f.row);
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.Write("@");
                     sleeptime--;
@@ -150,27 +150,27 @@ namespace RefactoringProject
                 else
                 {
                     // moving
-                    Position last = snakeelements.Dequeue();
+                    Pos last = se.Dequeue();
                     Console.SetCursorPosition(last.col, last.row);
                     Console.Write(" ");
                 }
 
-                if (Environment.TickCount - lastFoodTime >= FoodDisspearTime)
+                if (Environment.TickCount - lft >= FoodDisspearTime)
                 {
                     negativePoints = negativePoints + 50;
-                    Console.SetCursorPosition(food.col, food.row);
+                    Console.SetCursorPosition(f.col, f.row);
                     Console.Write(" ");
                     do
                     {
                         // feeding yhe snake 
-                        food = new Position(randomNubersGenerator.Next(0, Console.WindowHeight),
-                        randomNubersGenerator.Next(0, Console.WindowWidth));
+                        f = new Pos(rng.Next(0, Console.WindowHeight),
+                        rng.Next(0, Console.WindowWidth));
                     }
-                    while (snakeelements.Contains(food));
-                    lastFoodTime = Environment.TickCount;
+                    while (se.Contains(f));
+                    lft = Environment.TickCount;
                 }
 
-                Console.SetCursorPosition(food.col, food.row);
+                Console.SetCursorPosition(f.col, f.row);
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.Write("@");
 
